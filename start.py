603c78f97b81503bf3a9560bb09affcf468af084
Michael Mican <michael.mican@stud.th-deg.de>
2020-02-11 20:05:38 +0100

    add choosable time between capslock clicks

--- start.py
+++ start.py
@@ -5,8 +5,19 @@ import time
 
 keyboard = Controller()
 
+timeInSec = 0
+while(timeInSec <= 0):
+    try:
+        timeInMs = input("Enter time between capslock presses in milliseconds (250ms recommended)\n")
+        timeInSec = int(timeInMs)/1000.0
+        break
+    except:
+        print("\nPlease enter a valid integer (not negative && only numbers)\n")
+
+print("To stop the script just hit Ctrl + C")
+
 while True: 
     keyboard.press(Key.caps_lock)
     keyboard.release(Key.caps_lock)
-    time.sleep(0.1)
+    time.sleep(timeInSec)
     
